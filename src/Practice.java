4980a2ffb00e8f89662a3d10750732554712efd5
Masood Tanai <masood.tanai@gmail.com>
2022-10-05 15:18:38 -0700

    today's change

--- src/Practice.java
+++ src/Practice.java
@@ -1,9 +1,22 @@
+import java.awt.color.ICC_ColorSpace;
 
 public class Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a = 22;
+		int b= 33;
+		int c = a+b;
+		System.out.println(c);
+		
+		
+		
+		
+		
+		
+		
 	}
 
+	
 }
