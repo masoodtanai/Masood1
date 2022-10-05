0dca9f0f0d4ca67511fd0ea00941e4eda1fed2c4
Masood Tanai <masood.tanai@gmail.com>
2022-10-05 15:38:24 -0700

    NEW change

--- src/Practice.java
+++ src/Practice.java
@@ -5,9 +5,9 @@ public class Practice {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int a = 22;
+		int a = 72;
 		int b= 33;
-		int c = a+b;
+		int c = a-b;
 		System.out.println(c);
 		
 		
