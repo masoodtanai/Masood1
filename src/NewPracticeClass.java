113fd1c43f6f174ed84eb90389e527ec556c8d74
Masood Tanai <masood.tanai@gmail.com>
2022-10-05 15:35:30 -0700

    new file added NEW CLASS

---
+++ src/NewPracticeClass.java
@@ -0,0 +1,12 @@
+
+public class NewPracticeClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		System.out.println("I Like coding with Java");
+	}
+
+}
